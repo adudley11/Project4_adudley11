9ea0a9d43c91cee05df0edbe7565f23f7cfe3ac2
adudley11 <adudley11@student.cscc.edu>
2026-03-05 09:59:27 -0500

    Copied Project 3 and added given Project 4 code

---
+++ Player.py
@@ -0,0 +1,19 @@
+from CollideObjectBase import SphereCollideObject
+from panda3d.core import Loader, NodePath, Vec3
+from direct.task.Task import TaskManager
+from typing import Callable
+from direct.task import Task
+
+class Spaceship(SphereCollideObject):
+    def __init__(self, loader: Loader, taskmgr: TaskManager, accept: Callable[[str, Callable], None], modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        self.taskManager = taskMgr
+        self.render = parentNode  
+        self.SetKeyBindings()      
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
